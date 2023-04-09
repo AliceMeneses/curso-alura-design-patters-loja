03607bf4181cf0ea158d77af16851a8c7def850d
AliceMeneses <alicesilvademeneses@gmail.com>
2023-04-09 16:25:19 -0300

    Adicionar calculadora de impostos usando o padrão Strategy

---
+++ src/br/com/alura/loja/TestesImposto.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class TestesImposto {
+
+	public static void main(String[] args) {
+		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+		
+		System.out.println(calculadora.calcular(orcamento, new ICMS()));
+		
+	}
+	
+}
